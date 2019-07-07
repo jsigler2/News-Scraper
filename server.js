54a22e7472a56d9d3a91d15b0e21db15bc57fcf5
Jenna Sigler <jennasigler@Jennas-MacBook-Pro.local>
2019-07-06 21:38:20 -0700

    committing updated to routes.js and style.css

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
 
 var PORT = process.env.PORT || 3000;
 
@@ -6,9 +9,25 @@ var app = express();
 
 var router = express.Router();
 
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(router);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT);
